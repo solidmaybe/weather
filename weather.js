fc5b124ce824dbe1978461f17643a612d068f5d0
Jeffrey J. Bravo <37315038+jeff-bravo@users.noreply.github.com>
2018-08-25 14:51:33 -0700

    Pulled from /solidmaybe repo

---
+++ weather.js
@@ -0,0 +1,408 @@
+const weatherData = {
+   "message": "accurate",
+   "cod": "200",
+   "count": 10,
+   "list": [
+       {
+           "id": 5377100,
+           "name": "Nipomo",
+           "coord": {
+               "lat": 35.0428,
+               "lon": -120.476
+           },
+           "main": {
+               "temp": 58.24,
+               "pressure": 1014,
+               "humidity": 87,
+               "temp_min": 55.4,
+               "temp_max": 60.8
+           },
+           "dt": 1534258560,
+           "wind": {
+               "speed": 3.49,
+               "deg": 338.504
+           },
+           "sys": {
+               "country": ""
+           },
+           "rain": null,
+           "snow": null,
+           "clouds": {
+               "all": 90
+           },
+           "weather": [
+               {
+                   "id": 701,
+                   "main": "Mist",
+                   "description": "mist",
+                   "icon": "50d"
+               }
+           ]
+       },
+       {
+           "id": 5324802,
+           "name": "Arroyo Grande",
+           "coord": {
+               "lat": 35.1186,
+               "lon": -120.5908
+           },
+           "main": {
+               "temp": 58.62,
+               "pressure": 1014,
+               "humidity": 87,
+               "temp_min": 57.2,
+               "temp_max": 60.8
+           },
+           "dt": 1534258560,
+           "wind": {
+               "speed": 5.82,
+               "deg": 130
+           },
+           "sys": {
+               "country": ""
+           },
+           "rain": null,
+           "snow": null,
+           "clouds": {
+               "all": 90
+           },
+           "weather": [
+               {
+                   "id": 701,
+                   "main": "Mist",
+                   "description": "mist",
+                   "icon": "50d"
+               }
+           ]
+       },
+       {
+           "id": 5354552,
+           "name": "Grover Beach",
+           "coord": {
+               "lat": 35.1216,
+               "lon": -120.6213
+           },
+           "main": {
+               "temp": 58.62,
+               "pressure": 1014,
+               "humidity": 87,
+               "temp_min": 57.2,
+               "temp_max": 60.8
+           },
+           "dt": 1534258560,
+           "wind": {
+               "speed": 5.82,
+               "deg": 130
+           },
+           "sys": {
+               "country": ""
+           },
+           "rain": null,
+           "snow": null,
+           "clouds": {
+               "all": 90
+           },
+           "weather": [
+               {
+                   "id": 701,
+                   "main": "Mist",
+                   "description": "mist",
+                   "icon": "50d"
+               }
+           ]
+       },
+       {
+           "id": 5378763,
+           "name": "Oceano",
+           "coord": {
+               "lat": 35.0989,
+               "lon": -120.6124
+           },
+           "main": {
+               "temp": 58.62,
+               "pressure": 1014,
+               "humidity": 87,
+               "temp_min": 57.2,
+               "temp_max": 60.8
+           },
+           "dt": 1534258560,
+           "wind": {
+               "speed": 5.82,
+               "deg": 130
+           },
+           "sys": {
+               "country": ""
+           },
+           "rain": null,
+           "snow": null,
+           "clouds": {
+               "all": 90
+           },
+           "weather": [
+               {
+                   "id": 701,
+                   "main": "Mist",
+                   "description": "mist",
+                   "icon": "50d"
+               }
+           ]
+       },
+       {
+           "id": 5383431,
+           "name": "Pismo Beach",
+           "coord": {
+               "lat": 35.1428,
+               "lon": -120.6413
+           },
+           "main": {
+               "temp": 58.62,
+               "pressure": 1014,
+               "humidity": 87,
+               "temp_min": 57.2,
+               "temp_max": 60.8
+           },
+           "dt": 1534258560,
+           "wind": {
+               "speed": 5.82,
+               "deg": 130
+           },
+           "sys": {
+               "country": ""
+           },
+           "rain": null,
+           "snow": null,
+           "clouds": {
+               "all": 90
+           },
+           "weather": [
+               {
+                   "id": 701,
+                   "main": "Mist",
+                   "description": "mist",
+                   "icon": "50d"
+               }
+           ]
+       },
+       {
+           "id": 5332963,
+           "name": "Callender",
+           "coord": {
+               "lat": 35.053,
+               "lon": -120.5963
+           },
+           "main": {
+               "temp": 58.06,
+               "pressure": 1014,
+               "humidity": 87,
+               "temp_min": 55.4,
+               "temp_max": 60.8
+           },
+           "dt": 1534258560,
+           "wind": {
+               "speed": 5.82,
+               "deg": 130
+           },
+           "sys": {
+               "country": ""
+           },
+           "rain": null,
+           "snow": null,
+           "clouds": {
+               "all": 90
+           },
+           "weather": [
+               {
+                   "id": 701,
+                   "main": "Mist",
+                   "description": "mist",
+                   "icon": "50d"
+               }
+           ]
+       },
+       {
+           "id": 5392329,
+           "name": "San Luis Obispo County",
+           "coord": {
+               "lat": 35.3666,
+               "lon": -120.5344
+           },
+           "main": {
+               "temp": 59.04,
+               "pressure": 1014,
+               "humidity": 87,
+               "temp_min": 57.2,
+               "temp_max": 60.8
+           },
+           "dt": 1534258560,
+           "wind": {
+               "speed": 5.82,
+               "deg": 130
+           },
+           "sys": {
+               "country": ""
+           },
+           "rain": null,
+           "snow": null,
+           "clouds": {
+               "all": 90
+           },
+           "weather": [
+               {
+                   "id": 701,
+                   "main": "Mist",
+                   "description": "mist",
+                   "icon": "50d"
+               }
+           ]
+       },
+       {
+           "id": 5392323,
+           "name": "San Luis Obispo",
+           "coord": {
+               "lat": 35.2827,
+               "lon": -120.6597
+           },
+           "main": {
+               "temp": 58.53,
+               "pressure": 1014,
+               "humidity": 87,
+               "temp_min": 57.2,
+               "temp_max": 60.8
+           },
+           "dt": 1534258560,
+           "wind": {
+               "speed": 5.82,
+               "deg": 130
+           },
+           "sys": {
+               "country": ""
+           },
+           "rain": null,
+           "snow": null,
+           "clouds": {
+               "all": 90
+           },
+           "weather": [
+               {
+                   "id": 701,
+                   "main": "Mist",
+                   "description": "mist",
+                   "icon": "50d"
+               }
+           ]
+       },
+       {
+           "id": 5372954,
+           "name": "Miles",
+           "coord": {
+               "lat": 35.1855,
+               "lon": -120.7033
+           },
+           "main": {
+               "temp": 58.62,
+               "pressure": 1014,
+               "humidity": 87,
+               "temp_min": 57.2,
+               "temp_max": 60.8
+           },
+           "dt": 1534258560,
+           "wind": {
+               "speed": 5.82,
+               "deg": 130
+           },
+           "sys": {
+               "country": ""
+           },
+           "rain": null,
+           "snow": null,
+           "clouds": {
+               "all": 90
+           },
+           "weather": [
+               {
+                   "id": 701,
+                   "main": "Mist",
+                   "description": "mist",
+                   "icon": "50d"
+               }
+           ]
+       },
+       {
+           "id": 5393180,
+           "name": "Santa Maria",
+           "coord": {
+               "lat": 34.953,
+               "lon": -120.4358
+           },
+           "main": {
+               "temp": 58.24,
+               "pressure": 1014,
+               "humidity": 87,
+               "temp_min": 55.4,
+               "temp_max": 60.8
+           },
+           "dt": 1534258560,
+           "wind": {
+               "speed": 3.49,
+               "deg": 338.504
+           },
+           "sys": {
+               "country": ""
+           },
+           "rain": null,
+           "snow": null,
+           "clouds": {
+               "all": 90
+           },
+           "weather": [
+               {
+                   "id": 701,
+                   "main": "Mist",
+                   "description": "mist",
+                   "icon": "50d"
+               }
+           ]
+       }
+   ]
+};
+let cityName = "Arroyo Grande";
+
+//document.getElementById("city_02").addEventListener("click", gitCity(document.getElementById("city_02").innerHTML));
+// document.getElementById("city_03").addEventListener("click", gitCity(document.getElementById("city_03").innerHTML));
+// document.getElementById("city_04").addEventListener("click", gitCity(document.getElementById("city_04").innerHTML));
+// document.getElementById("city_05").addEventListener("click", gitCity(document.getElementById("city_05").innerHTML));
+// document.getElementById("city_06").addEventListener("click", gitCity(document.getElementById("city_06").innerHTML));
+// document.getElementById("city_07").addEventListener("click", gitCity(document.getElementById("city_07").innerHTML));
+// document.getElementById("city_08").addEventListener("click", gitCity(document.getElementById("city_08").innerHTML));
+// document.getElementById("city_09").addEventListener("click", gitCity(document.getElementById("city_09").innerHTML));
+// document.getElementById("city_10").addEventListener("click", gitCity(document.getElementById("city_10").innerHTML));
+
+
+function weatherApp(obj, cityName1){
+
+   for(let i = 0; i < obj.list.length; i++){
+       if(obj.list[i].name === cityName1){
+           let cityTemp = obj.list[i].main.temp;
+            document.getElementById("temperatureID").innerHTML = cityTemp;
+            let cityTempMin = obj.list[i].main.temp_min;
+            document.getElementById("tempLowID").innerHTML = cityTempMin;
+            let cityTempMax = obj.list[i].main.temp_max;
+            document.getElementById("tempHighID").innerHTML = cityTempMax;
+       //     let cityWeather = obj.list[i].weather[0].main;
+       //     document.getElementById("").innerHTML = cityWeather;
+            let cityHumidity = obj.list[i].main.humidity;
+            document.getElementById("humidityID").innerHTML = cityHumidity;
+           document.getElementById("city_name").innerHTML = cityName1 + ", CA";
+       }
+   }
+}
+
+function gitCity(){
+   // cityName = city;
+   // weatherApp (weatherData, city);
+   let x = document.getElementById("mySelect").value;
+   weatherApp(weatherData, x);
+}
+
+weatherApp(weatherData, cityName);
+
